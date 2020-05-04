7b8dbe59293a33289613af5072bab4336c1d674f
Boris <boris021@mail.ru>
2020-05-04 13:04:24 +0500

    create cleanCSS & miniCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require("gulp");
 const browserSync = require("browser-sync").create();
+const cleanCSS = require("gulp-clean-css");
+const cssmin = require("gulp-cssmin");
+const rename = require("gulp-rename");
 
 gulp.task("hello", function (done) {
   console.log("Привет, мир!");
@@ -14,4 +17,42 @@ gulp.task("browser-sync", function () {
     },
   });
   gulp.watch("./*.html").on("change", browserSync.reload);
+});
+
+gulp.task("minify-css", () => {
+  return gulp
+    .src("css/*.css")
+    .pipe(
+      cleanCSS({
+        compatibility: "ie8",
+      })
+    )
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("minify-css", () => {
+  return gulp
+    .src("css/*.css")
+    .pipe(
+      cleanCSS({
+          debug: true,
+        },
+        (details) => {
+          console.log(`${details.name}: ${details.stats.originalSize}`);
+          console.log(`${details.name}: ${details.stats.minifiedSize}`);
+        }
+      )
+    )
+    .pipe(gulp.dest("dist"));
+});
+
+//.min-css
+gulp.task("mincss", function () {
+  gulp
+    .src("css/*.css")
+    .pipe(cssmin())
+    .pipe(rename({
+      suffix: ".min"
+    }))
+    .pipe(gulp.dest("dist"));
 });
